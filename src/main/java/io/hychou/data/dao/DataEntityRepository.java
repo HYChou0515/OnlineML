75c9751ed98e4f0a3bdcbe53b4f38768c1ee2ae5
Chou Hung-Yi <s1243221@gmail.com>
2018-12-14 03:28:33 +0800

    remove bug of delete method in CrudRepository

--- src/main/java/io/hychou/data/dao/DataEntityRepository.java
+++ src/main/java/io/hychou/data/dao/DataEntityRepository.java
@@ -2,7 +2,10 @@ package io.hychou.data.dao;
 
 import io.hychou.data.entity.DataEntity;
 import io.hychou.data.entity.DataInfo;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -11,5 +14,7 @@ public interface DataEntityRepository extends CrudRepository<DataEntity, String>
     List<DataInfo> findDataInfoBy();
     Optional<DataEntity> findByName(String name);
     boolean existsByName(String name);
+    @Modifying
+    @Transactional
     void deleteByName(String name);
 }
